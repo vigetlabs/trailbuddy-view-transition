64724e3dd5e147c9b80dcc0f1f369fa6e64b36e4
Eric <ekfuhrmann@gmail.com>
2023-09-29 17:45:51 -0600

    Setting up nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      black: '#000000',
+      'pearl-brush': '#E2DDD1',
+      'quill-gray': '#DBE0DA',
+      champagne: '#F3E6C4',
+      'pale-turquoise': '#B4F0F0',
+      matisse: '#366A9C',
+      orient: '#2C6488',
+      shark: '#1D252D',
+      'red-orange': '#FF4237',
+    },
+    extend: {},
+  },
+  plugins: [],
 }
